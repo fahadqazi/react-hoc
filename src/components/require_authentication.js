b99ff6a8dbe7e3d268db3fe218cf9042828d807c
Fahad Qazi <fahadqazi@gmail.com>
2017-05-30 23:17:25 +0100

    authentication based routing

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,9 +3,25 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent){
     class Authentication extends Component{
+        static contextTypes = {
+          router: React.PropTypes.object
+        }
+
+        componentWillMount(){
+            if(!this.props.authenticated){
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps){
+            if(!nextProps.authenticated){
+                this.context.router.push('/');
+            }
+        }
+
         render(){
-            console.log('Component: ', ComposedComponent);
-            console.log(this.props.authenticated);
+            console.log("context ", this.context)
+
             return <ComposedComponent {...this.props} />
         }
     }
